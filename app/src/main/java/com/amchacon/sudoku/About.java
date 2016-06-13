417f814f1299bd0a9ea9363001ca7aa020d46a04
amchacon <miachm3@gmail.com>
2016-06-13 13:49:51 +0200

    Added About's activity

---
+++ app/src/main/java/com/amchacon/sudoku/About.java
@@ -0,0 +1,16 @@
+package com.amchacon.sudoku;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by MiguelPC on 13/06/2016.
+ */
+public class About extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aboutme);
+    }
+}
